cd2314efa3f23b05ebe2b29d0bfdbae6ef24b13e
Nicky Shah <nickshah534@gmail.com>
2024-01-12 13:17:45 +1100

    New Razor page project has been added and creat functionality has been added

---
+++ BulkyWeb_Razor/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,28 @@
+using BulkyWeb_Razor.Data;
+using BulkyWeb_Razor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWeb_Razor.Pages.Categories
+{
+    [BindProperties]
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
+        }
+    }
+}
---
+++ BulkyWeb_Razor/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWeb_Razor.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
