7a846c6cb08e54c7ad8967ebab6c58af6ac157f5
Nicky Shah <nickshah534@gmail.com>
2024-01-19 08:33:23 +1100

    vbn

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+//builder.Services.AddRazorPages();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
@@ -34,6 +35,8 @@ app.UseRouting();
 app.UseAuthentication();
 
 app.UseAuthorization();
+
+//app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
