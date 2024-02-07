47d16067c08cc0459f33f044d8594c2f779140c2
Nicky Shah <nickshah534@gmail.com>
2024-02-07 12:15:45 +1100

    DbInitializer is added and session in cart is cleared

---
+++ Bulky.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,35 @@
+﻿using Bulky.DataAccess.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace Bulky.DataAccess.DbInitializer
+{
+    internal class DbInitializer : IDbInitializer
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _db;
+
+        public DbInitializer(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _db = db;
+        }
+
+        public void Initialize()
+        {
+
+
+
+            //Migrations if they are not applied
+
+
+            // Create roles if they are not created
+
+
+            // If roles are not created, then we will create admin user as well
+
+
+        }
+    }
+}
